a3dbc2ed7a01f81d4b93ab73d0f2f60b8aae11f0
WALEED IFTIKHAR <waleediftikhar188@gmail.com>
2025-10-29 15:21:09 +0500

    add fields

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -18,11 +18,12 @@ const Contact = () => {
     const message = e.target[3].value;
 
     try {
-      const res = await fetch("https://portfolio-api-three-gamma.vercel.app/api/send-message", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name, email, message }),
-      });
+     const res = await fetch("https://portfolio-api-three-gamma.vercel.app/api/send-message", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ name, email, subject, message }),
+});
+
 
       const data = await res.json();
       if (res.ok) {
